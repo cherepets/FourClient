4a21ae582d7b001cb7609b016eecaf58543f11d4
Гавриков Борис Витальевич <b.gavrikov@t1-group.ru>
2016-03-01 18:51:22 +0300

    Added RemindToast.xaml.cs

---
+++ Universal Windows Platform/FourClient.Library/Notifications/RemindToast.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace FourClient.Library.Notifications
+{
+    public sealed partial class RemindToast
+    {
+        public void RegenerateDummy()
+        {
+            InitializeComponent();
+            var vm = ViewModelHelper.GenerateDummy(new RemindToastViewModel());
+            DataContext = vm;
+            Settings.Current.DummyRemindToast = CreateNotification().Content.GetXml();
+        }
+
+        public ToastNotification CreateBackgroundNotification()
+        {
+            var vm = DataContext as RemindToastViewModel;
+            var xml = ViewModelHelper.FillXml(vm, Settings.Current.DummyRemindToast);
+            var doc = new XmlDocument();
+            doc.LoadXml(xml);
+            return new ToastNotification(doc);
+        }
+    }
+}
